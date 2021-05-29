0287fdc059afe7cff80d310aa3904bf21cdeffe8
Gayathri5151 <73778830+Gayathri5151@users.noreply.github.com>
2021-05-29 11:47:48 +0530

    Add files via upload

---
+++ Project-43/js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "COLD";
+const accessCode2 = "COIN";
+const accessCode3 = "TEAPOT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text(" Hint: O D L C", 100,50)
+    fill("lightblue")
+    text(" You can catch it, but cannot throw it!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+  
+fill("white")
+textSize(15)
+text(" Hint: I O N C", 700,150)
+fill("lightblue")
+text("It has a head and a tail but no body!", 700,170)
+
+fill("white")
+textSize(15)
+text("Hint: P T A T O E", 100,250)
+fill("lightblue")
+text("Starts with a ‘T’ ends with a ‘T’ and has T in it!", 100,270)
+    
+
+}
